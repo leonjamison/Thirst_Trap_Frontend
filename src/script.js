52755cff9ffc45f7b0416333ff13cd5c214519f1
Leon Jamison <leonjamison@icloud.com>
2020-05-06 12:17:54 -0400

    add likes and style

--- src/script.js
+++ src/script.js
@@ -1,11 +1,18 @@
 const endPoint = `http://localhost:3000/drinks`
+const reviewUrl = `http://localhost:3000/reviews`
+
 
 
 document.addEventListener('DOMContentLoaded', ()=> {
 
 
 renderDrinks()
+getReviewObj()
     
+
+
+    
+
 
 
 
@@ -28,19 +35,53 @@ function viewDrinks(drink){
     // console.log(drink)
     let div = document.getElementById('drink-collection')
     let drinkDiv = document.createElement('div')
+    drinkDiv.dataset.id = drink.id
     drinkDiv.innerHTML = 
     `
-    <div class = 'card'>
+    <div style="display: table-cell;" class='card'>
     <h2>${drink.name}</h2>
     <img src=${drink.img_url} class="drink-avatar" />
-    <button class="like-btn">Like </button> 
+    <button class="like-btn">0 Likes</button> 
     </div>
     `
     div.append(drinkDiv)
 }
 
+function getReviewObj(){
+    fetch(reviewUrl)
+    .then(resp => resp.json())
+    .then(reviews => reviews.forEach((review)=>{
+        addLikes(review)
+    }) )
 
 
+}
+
+function addLikes(review){
+    console.log(review)
+
+    // document.addEventListener('click', (e)=>{
+
+    //     // if(e.target.className === 'like-btn' ){
+    //     //     console.log(likes.likes)
+            
+    //     //     let likeBtn = e.target
+    //     //     // debugger
+    //     //     // // console.log(likeBtn.textContent[0])
+    //     //     likeBtn.textContent = `${likes.likes} Likes`
+    //     //     // let likes = parseInt(likeBtn.textContent[0])
+
+
+    //     //     // likeBtn.textContent = `${likes.likes} likes`
+           
+    //     //     // console.log(likeBtn)
+            
+    //     // }
+
+
+    // })
+
+}
 
 
 
